89df21bd0a3922396513de92e8ed92a2a77b9be5
aldrinpvincent-moengage <aldrin.vincet@moengage.com>
2022-07-12 23:48:37 +0530

    server

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
-const port = 3124
+const PORT = process.env.PORT || 8080;
+
 
 app.use(express.static('public'))
 
@@ -8,6 +9,4 @@ app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+app.listen(PORT, () => console.log(`Server is running in port ${PORT}`));
