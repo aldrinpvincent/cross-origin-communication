c49f431ffa46a2ad6ba945e79b88b6af94038536
aldrinpvincent-moengage <aldrin.vincet@moengage.com>
2022-07-12 23:55:21 +0530

    vercel

--- server/api/index.js
+++ server/api/index.js
@@ -3,7 +3,7 @@ const app = express()
 const PORT = process.env.PORT || 8080;
 
 
-app.use(express.static('public'))
+// app.use(express.static('public'))
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
